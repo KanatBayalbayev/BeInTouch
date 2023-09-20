a5c569e4a8c04d278534b84c4158268ac4b3c697
Kanat Bayalbayev <Kanatkz07@mail.ru>
2023-09-20 14:08:13 +0600

    created SignUp and Reset layouts

--- app/src/main/java/com/example/beintouch/fragments/Login.kt
+++ app/src/main/java/com/example/beintouch/fragments/Login.kt
@@ -14,13 +14,25 @@ class Login : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentLoginBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.buttonToSignUp.setOnClickListener {
+            fragmentManager?.beginTransaction()
+                ?.replace(R.id.container, Registration.newInstance())
+                ?.addToBackStack(null)
+                ?.commit()
+        }
+        binding.buttonToOpenReset.setOnClickListener {
+            fragmentManager?.beginTransaction()
+                ?.replace(R.id.container, Reset.newInstance())
+                ?.addToBackStack(null)
+                ?.commit()
+        }
 
     }
 
--- app/src/main/java/com/example/beintouch/fragments/Reset.kt
+++ app/src/main/java/com/example/beintouch/fragments/Reset.kt
@@ -6,31 +6,32 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.beintouch.R
-import com.example.beintouch.databinding.FragmentLoginBinding
 import com.example.beintouch.databinding.FragmentRegistrationBinding
+import com.example.beintouch.databinding.FragmentResetBinding
 
 
-class Registration : Fragment() {
-    private lateinit var binding: FragmentRegistrationBinding
+class Reset : Fragment() {
+    private lateinit var binding: FragmentResetBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentRegistrationBinding.inflate(inflater, container, false)
+    ): View? {
+        binding = FragmentResetBinding.inflate(inflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.buttonToBackToLoginFromSignUp.setOnClickListener {
+        binding.buttonToBackToLoginFromReset.setOnClickListener {
             fragmentManager?.beginTransaction()
                 ?.replace(R.id.container, Login.newInstance())
                 ?.commit()
         }
     }
+
     companion object {
         @JvmStatic
-        fun newInstance() = Registration()
+        fun newInstance() = Reset()
     }
 }
